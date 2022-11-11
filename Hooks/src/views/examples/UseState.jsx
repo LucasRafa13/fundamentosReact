9ea8c43eb584b3f4cd4021c18d48266e660b7a5c
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-11 13:18:15 -0400

    feat: :fire:

--- Hooks/src/views/examples/UseState.jsx
+++ Hooks/src/views/examples/UseState.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react"
+import PageTitle from "../../components/layout/PageTitle"
 
-const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-        </div>
-    )
+const UseState = props => {
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+    </div>
+  )
 }
 
 export default UseState
