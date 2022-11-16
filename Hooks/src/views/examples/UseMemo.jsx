82763c1b7eb6d0177a937d529ed8d14e96c8aa50
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-15 20:50:20 -0400

    feat: :sparkles: useEffect

--- Hooks/src/views/examples/UseMemo.jsx
+++ Hooks/src/views/examples/UseMemo.jsx
@@ -4,7 +4,7 @@ import PageTitle from "../../components/layout/PageTitle"
 const UseMemo = props => {
   return (
     <div className="UseMemo">
-      <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memoizado!" />
+      <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memorizado!" />
     </div>
   )
 }
