2bc0233acf49f0bd38c7c57e56e75ab5b4182357
Lucas <lucasrafael123araujolima@gmail.com>
2022-12-12 08:00:00 -0400

    feat: :sparkles: Reducer é massa demais!

--- Hooks/src/views/examples/UseReducer.jsx
+++ Hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,42 @@
 import React from "react"
 import PageTitle from "../../components/layout/PageTitle"
 
+const initialState = {
+  cart: [],
+  products: [],
+  user: null,
+  number: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "number_add2":
+      return { ...state, number: state.number + 2 }
+    default:
+      return state
+  }
+}
+
 const UseReducer = props => {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+
   return (
     <div className="UseReducer">
       <PageTitle
         title="Hook UseReducer"
         subtitle="Uma outra forma de ter estado em componentes funcionais!"
       />
+      <div className="center">
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "number_add2" })}
+          >
+            +2
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
