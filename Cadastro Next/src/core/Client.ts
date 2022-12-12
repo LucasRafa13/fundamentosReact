e2411d61d4986d6f6dfd2a679e58cc279e0c1d4f
Lucas <lucasrafael123araujolima@gmail.com>
2022-12-12 09:28:38 -0400

    feat: :zap:

---
+++ Cadastro Next/src/core/Client.ts
@@ -0,0 +1,26 @@
+export default class Client {
+  id: string
+  name: string
+  age: number
+
+  constructor(name: string, age: number, id: string) {
+    this.name = name
+    this.age = age
+    this.id = id
+  }
+
+  static void() {
+    return new Client("", "", 0)
+  }
+
+  getId() {
+    return this.id
+  }
+
+  getName() {
+    return this.name
+  }
+  getAge() {
+    return this.age
+  }
+}
